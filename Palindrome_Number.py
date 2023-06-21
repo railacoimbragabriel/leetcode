8374cf739a0bb3b8a7820dbaff15387353527fe9
Raíla Coimbra Gabriel <136643336+railacoimbragabriel@users.noreply.github.com>
2023-06-21 18:41:39 -0300

    Palindrome_Number.py

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -1,10 +1,7 @@
-
-def isPalindrome(x: int) -> bool:
-    x_str = str(x)
-    if x_str == x_str[::-1]:
-        return True
-    else:
-        return False
-
-
-print(isPalindrome(10))
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x_str = str(x)
+        if x_str == x_str[::-1]:
+            return True
+        else:
+            return False
